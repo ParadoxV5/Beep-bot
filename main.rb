89f775b6833461e52ae2b1337f09894e0f694dfa
ParadoxV5 <paradox.ver5@gmail.com>
2020-12-29 19:45:34 -0700

    Commit 0

---
+++ main.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+require('csv')
+require('discordrb')
+
+DATA_PATH = File.expand_path("#{__FILE__}/../map.csv")
+def load_data
+  data = {}
+  CSV.foreach(DATA_PATH) do |replace_with, *regexps|
+    regexps.each do|regexp| data[Regexp.new(regexp, true)] = replace_with end
+  end
+  $data = data
+end
+
+load_data
+begin
+  require 'listen'
+  Listen.to("#{__FILE__}/../") do load_data if _1.any?(DATA_PATH) end.start
+rescue LoadError
+  warn('`listen` gem not found. You’ll have to restart Beep bot to manually reload `map.csv`.')
+end
+
+BOT = Discordrb::Bot.new(token: File.open("#{__FILE__}/../bot_token.txt") do _1.gets end)
+at_exit do BOT.stop end
+
+BOT.message do|event|
+  $text = event.text.dup
+  unless ($catches = $data.filter do|regexp, replace_with| $text.gsub!(regexp, replace_with) end).empty?
+    $event_message = event.message
+    $event_message.respond(
+      '> <@' + event.user.id.to_s + '>' + if $event_message.attachments.empty?
+        "\r\n"
+      else
+        "\t(File `" + $event_message.attachments.first.filename + "` has been lost in Discord.)\r\n"
+      end,
+      false, nil, nil, nil,
+      $event_message.referenced_message
+    )
+    $event_message.respond($text)
+    $event_message.delete('Beep bot triggered: ' + $catches.keys.join(' ')) # Join regexps with single space, ignore replace_with
+  end
+end
+
+begin
+  BOT.run
+rescue Interrupt
+end
